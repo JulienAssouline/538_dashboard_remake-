ed327e416253a0ca9d7cf90272ac00d07409b6a5
JulienAssouline <julien1993@hotmail.ca>
2019-03-18 14:15:02 -0400

    gitattributeignore

---
+++ endorsements/build/precache-manifest.7f7314ad4dedf3087b61b16e61818700.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "3ce23ba4027b2ee1a3f9",
+    "url": "/538_dashboard_remake-/static/css/main.b5cf3925.chunk.css"
+  },
+  {
+    "revision": "3ce23ba4027b2ee1a3f9",
+    "url": "/538_dashboard_remake-/static/js/main.f46baeb9.chunk.js"
+  },
+  {
+    "revision": "572d27f90dab7783f011",
+    "url": "/538_dashboard_remake-/static/js/runtime~main.a34e9260.js"
+  },
+  {
+    "revision": "d4ffbeb41add4618ed88",
+    "url": "/538_dashboard_remake-/static/js/2.19fa5661.chunk.js"
+  },
+  {
+    "revision": "9a60010b8dd16f990fbeb35c6eb15a61",
+    "url": "/538_dashboard_remake-/static/media/endorsements-2020.9a60010b.csv"
+  },
+  {
+    "revision": "f5ae6b7b1e4627fc1045f5c7912ab653",
+    "url": "/538_dashboard_remake-/static/media/AmyKlobuchar.f5ae6b7b.png"
+  },
+  {
+    "revision": "1a5a9ebfc1667a1fb11ea145902ddddf",
+    "url": "/538_dashboard_remake-/static/media/BernieSanders.1a5a9ebf.png"
+  },
+  {
+    "revision": "525285c23f1290a6d83d882e98b4774f",
+    "url": "/538_dashboard_remake-/static/media/CoryBooker.525285c2.png"
+  },
+  {
+    "revision": "210d511eafebc9ed4383bc9a5cd83335",
+    "url": "/538_dashboard_remake-/static/media/ElizabethWarren.210d511e.png"
+  },
+  {
+    "revision": "e8ca24b6f514f9c6f5cc54df7ddd5a41",
+    "url": "/538_dashboard_remake-/static/media/JayInslee.e8ca24b6.png"
+  },
+  {
+    "revision": "29e77d219d7a456b6ee5820079eaa697",
+    "url": "/538_dashboard_remake-/static/media/JoeBiden.29e77d21.png"
+  },
+  {
+    "revision": "f76e1a41c2c1b14902ed1bf35a932814",
+    "url": "/538_dashboard_remake-/static/media/JohnDelaney.f76e1a41.png"
+  },
+  {
+    "revision": "b63a90aba4696cb2e6397501c39a734f",
+    "url": "/538_dashboard_remake-/static/media/JulianCastro.b63a90ab.png"
+  },
+  {
+    "revision": "adc30979074a6cf819656370b03fd30d",
+    "url": "/538_dashboard_remake-/static/media/KamalaHarris.adc30979.png"
+  },
+  {
+    "revision": "6c235dd8c7b29c3e62eb009990cb9b1b",
+    "url": "/538_dashboard_remake-/static/media/MichaelBloomberg.6c235dd8.png"
+  },
+  {
+    "revision": "65ff0b9897c81eb968a876e02e26d0ce",
+    "url": "/538_dashboard_remake-/static/media/endorsements_analysed.65ff0b98.csv"
+  },
+  {
+    "revision": "05491f192521008b34e647edc6957e97",
+    "url": "/538_dashboard_remake-/static/media/position.05491f19.csv"
+  },
+  {
+    "revision": "4b89e017ced216b7280717d10082d981",
+    "url": "/538_dashboard_remake-/static/media/Screen Shot 2019-01-09 at 5.55.28 PM.4b89e017.png"
+  },
+  {
+    "revision": "e56c635d29b7c6c412e652001490d0b1",
+    "url": "/538_dashboard_remake-/index.html"
+  }
+];
---
+++ endorsements/build/service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/538_dashboard_remake-/precache-manifest.7f7314ad4dedf3087b61b16e61818700.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/538_dashboard_remake-/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
